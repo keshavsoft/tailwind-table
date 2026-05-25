3ca96148a2619f563c4c251a01da49b7d17fbf57
keshavsoft <nknnkumar@yahoo.com>
2026-05-25 22:23:33 +0530

    public to be tested

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // src/main.js
 
 (async () => {
-    window.KSHeaderVersion = "v5";
-    window.KSHeaderTemplateVersion = "v4";
+    window.KSHeaderVersion = "v2";
+    window.KSHeaderTemplateVersion = "v1";
 
     const module = await import(
-        `../bin/${window.KSHeaderVersion}/commands/header/template/${window.KSHeaderTemplateVersion}/initHeader.js`
+        `../bin/${window.KSHeaderVersion}/commands/table/template/${window.KSHeaderTemplateVersion}/initHeader.js`
     );
 
-    window.KSHeader = module.default;
+    window.KSTable = module.default;
 })();
